351f29ff073088001132c14dd956ab40077d4541
amir <amirmoradisa1381@gmail.com>
2023-01-23 15:22:01 +0330

    add titles , descriptions , links

--- portfolio.py
+++ portfolio.py
@@ -1,12 +1,13 @@
+import pandas
 import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 with col1:
     st.image("images/amir.png")
 with col2:
-    st.title("Amir Moradi")
+    st.title("Amir Moradi here!")
     content = """Hi, I'm Amir. I'm 20 years old , 
     I'm python programmer and interested in AI and robotics. 
     I study CE in university. I started programming from 2021 
@@ -14,3 +15,25 @@ with col2:
      cuz i like it and  it's quite simple and powerful """
 
     st.info(content)
+
+content = """
+below you can find some of the apps i have built in python feel free to
+ contact me!"""
+st.write(f"<h4>{content}</h4>",
+         unsafe_allow_html=True)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source code]({row['url']})")
