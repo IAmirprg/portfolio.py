8db8fbec48c32f43fe11023fc591e326af2e255d
amir <amirmoradisa1381@gmail.com>
2023-01-24 09:40:37 +0330

    design email form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Contact me")
+
+with st.form(key="my_form"):
+    user_email = st.text_input("Your Email address:")
+    user_message = st.text_area("Your message:")
+    submit_button = st.form_submit_button("Submit")
